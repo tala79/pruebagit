3350ae62852d0c89a3e8399fd12131d99a9a3110
tron <tron@DESKTOP-0GKO3J4>
2017-06-14 12:07:22 +0200

    Esta es la rama 1

--- src/pruebagit/Pruebagit.java
+++ src/pruebagit/Pruebagit.java
@@ -16,7 +16,7 @@ public class Pruebagit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Esta es la prueba de la versión 1");
+        System.out.println("Esta es la prueba de la rama 1");
     }
     
 }
