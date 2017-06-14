fad6479f788a9e971d13d1bfb5d0560c23167bca
tron <tron@DESKTOP-0GKO3J4>
2017-06-14 12:04:52 +0200

    Esta es la versión 1

---
+++ src/pruebagit/Pruebagit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebagit;
+
+/**
+ *
+ * @author tron
+ */
+public class Pruebagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esta es la prueba de la versión 1");
+    }
+    
+}
